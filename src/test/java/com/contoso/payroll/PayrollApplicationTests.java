26f2474f695333cc6af72db0a56b6234e17f7080
Drew Lewis <29957923+drewelewis@users.noreply.github.com>
2025-02-28 15:36:28 -0500

    initial commit

---
+++ src/test/java/com/contoso/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contoso.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
